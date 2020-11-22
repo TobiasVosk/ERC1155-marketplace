fc0ca740f916435069199a9854e83c707db22172
Tobias <tobiasvosk@gmail.com>
2020-11-21 22:39:51 -0300

    mintERC1155 script

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,14 +4,14 @@ const INFURA_PROJECT_ID = "1ff8201638bf41a7839d6c5e639b258e";
 
 const ROPSTEN_PRIVATE_KEY = "f9f2f5b27344a6cf70a23b7c722e0e24ad976edf16556fbf452e706b8185e9f6";
 
+const SECOND_ACCOUNT_PK = "835fdac3de5a4ba8fa70e631e1d4bd8a336bb944b7a96c5d668ac1278ed13942"
+
 module.exports = {
-  solidity: {
-    version: '0.7.4',
-  },
+  solidity: '0.7.4',
   networks: {
     ropsten: {
       url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
-      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`, `0x${SECOND_ACCOUNT_PK}`]
     }
   }
 };
---
+++ scripts/mintERC1155.js
@@ -0,0 +1,23 @@
+const config = require('../config.json');
+const e18 = ethers.BigNumber.from(10).pow(18);
+
+
+async function main() {
+
+    const [deployer, acc2] = await ethers.getSigners();
+  
+    console.log(
+      "Minting ERC1155 to address:",
+      acc2.address
+    );
+
+    const tokenERC1155 = await ethers.getContractAt('TokenERC1155', config.contracts.ropsten.erc1155.address, acc2);
+    await tokenERC1155.connect(acc2).mint({from: acc2.address, value: ethers.utils.parseEther('1')});
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch(error => {
+      console.error(error);
+      process.exit(1);
+    });
